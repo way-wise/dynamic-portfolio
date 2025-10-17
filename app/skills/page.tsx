052b6366f583667790d4fddaaa1835594fa16ca3
rakib0101 <rakib.01jan@gmail.com>
2025-10-17 19:52:22 +0600

    UI updated for hero section

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,11 +1,13 @@
 import { Skills } from "@/components/skills"
 import { Navigation } from "@/components/navigation"
+import { Experience } from "@/components/experience"
 
 export default function SkillsPage() {
   return (
     <div className="min-h-screen">
       <Navigation />
       <main>
+        <Experience />
         <Skills />
       </main>
     </div>
